1b1369bcfd42b9548b8c21ac063e723a22e5b171
Victor Ramos <vp.ramos@gmail.com>
2025-09-23 11:35:29 -0300

    wip: add pointer idle

--- playground/lib/examples/platform_game.dart
+++ playground/lib/examples/platform_game.dart
@@ -9,6 +9,7 @@ import 'package:lepiengine/engine/core/collision_manager.dart';
 import 'package:lepiengine/engine/core/game_object.dart';
 import 'package:lepiengine/engine/core/input_manager.dart';
 import 'package:lepiengine/engine/core/scene.dart';
+import 'package:lepiengine/engine/core/scene_manager.dart';
 import 'package:lepiengine/engine/game_objects/sprite_sheet.dart';
 import 'package:lepiengine/engine/game_objects/tilemap.dart';
 import 'package:lepiengine/engine/models/tileset.dart';
@@ -26,18 +27,30 @@ class PlatformGame extends Scene {
 
     _loadPlayer();
 
+    _loadPointerIdle();
+
     setLayerOrder("map", 0);
 
     // AudioManager.instance.playMusic(Constants.backgroundMusic);
 
-    add(DebugText(text: 'Debug Text', position: const Offset(100, 200)));
+    // add(DebugText(text: 'Debug Text', position: const Offset(100, 200)));
+  }
+
+  Future<void> _loadPointerIdle() async {
+    final pointerIdleSprite = await AssetLoader.loadImage(
+      Constants.pointerIdle,
+    );
+    final pointerIdle = PointerIdle(image: pointerIdleSprite);
+    pointerIdle.position = const Offset(100, 450);
+    add(pointerIdle, layer: 'frontPlayer');
+    setLayerOrder("frontPlayer", 4);
   }
 
   Future<void> _loadPlayer() async {
     final playerSprite = await AssetLoader.loadImage(Constants.character);
     final player = Player(image: playerSprite);
-    player.position = const Offset(650, 400);
     player.size = const Size(48, 48);
+    player.position = const Offset(250, 400);
     add(player);
     player.play('idle');
     setLayerOrder("entities", 3);
@@ -124,7 +137,7 @@ class Player extends SpriteSheet with PhysicsBody, CollisionCallbacks {
   Player({super.name = 'Player', required super.image}) : super() {
     addAABBCollider(
       size: Size(48, 48),
-      anchor: ColliderAnchor.topLeft,
+      anchor: ColliderAnchor.bottomCenter,
       debugColor: Colors.blue,
     );
 
@@ -132,8 +145,6 @@ class Player extends SpriteSheet with PhysicsBody, CollisionCallbacks {
     maxFallSpeed = 500;
   }
 
-  double coyoteTime = 0.0;
-  final double maxCoyoteTime = 0.1;
   bool isGrounded = false;
   final double moveSpeed = 150.0;
   final double jumpForce = -350.0;
@@ -147,14 +158,6 @@ class Player extends SpriteSheet with PhysicsBody, CollisionCallbacks {
   @override
   void update(double dt) {
     super.update(dt);
-
-    // Atualiza coyote time (lógica específica de plataforma)
-    if (isGrounded) {
-      coyoteTime = maxCoyoteTime;
-    } else {
-      coyoteTime = (coyoteTime - dt).clamp(0.0, maxCoyoteTime);
-    }
-
     _handleInput();
   }
 
@@ -170,7 +173,6 @@ class Player extends SpriteSheet with PhysicsBody, CollisionCallbacks {
         AudioManager.instance.playSound('jump.mp3');
         play('jump');
         setVelocity(Offset(velocity.dx, jumpForce));
-        coyoteTime = 0.0;
         isGrounded = false;
         return;
       } else {}
@@ -212,7 +214,7 @@ class Player extends SpriteSheet with PhysicsBody, CollisionCallbacks {
     // Removido: não marca grounded só por colidir com Tilemap; usa lado.
 
     if (collision.selfSide == CollisionSide.bottom) {
-      debugPrint("collision bottom: ${collision.selfSide}");
+      SceneManager.instance.current?.camera.lightShake();
     } else if (collision.selfSide == CollisionSide.top) {
       debugPrint("collision top: ${collision.selfSide}");
     } else if (collision.selfSide == CollisionSide.left) {
@@ -343,5 +345,41 @@ class Player extends SpriteSheet with PhysicsBody, CollisionCallbacks {
         frames: [Frame(col: 0, row: row)],
       ),
     );
+  }
+}
+
+class PointerIdle extends SpriteSheet {
+  PointerIdle({
+    super.name = 'PointerIdle',
+    super.size = const Size(64, 64),
+    required super.image,
+  }) : super() {
+    addAnimation(
+      SpriteAnimation(
+        name: 'idle',
+        frameSize: Size(48, 48),
+        frames: [
+          Frame(col: 0, row: 0),
+          Frame(col: 0, row: 0),
+          Frame(col: 0, row: 0),
+          Frame(col: 0, row: 0),
+          Frame(col: 0, row: 0),
+          Frame(col: 0, row: 0),
+          Frame(col: 1, row: 0),
+          Frame(col: 2, row: 0),
+          Frame(col: 3, row: 0),
+          Frame(col: 4, row: 0),
+          Frame(col: 5, row: 0),
+          Frame(col: 6, row: 0),
+        ],
+        frameDuration: 0.2,
+      ),
+    );
+  }
+
+  @override
+  void onAdd() {
+    super.onAdd();
+    play('idle');
   }
 }
